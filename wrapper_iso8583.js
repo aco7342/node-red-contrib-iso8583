a2859133fe7a508360e717d08284795e1c4c8660
antonio carlos oliveira <acoliveira@terra.com.br>
2017-08-17 02:38:08 -0300

    set reentrant mode

--- wrapper_iso8583.js
+++ wrapper_iso8583.js
@@ -12,12 +12,37 @@ module.exports = function(RED) {
                        
             if (!msgS2T) { // monta iso8583 com array, devolvendo string
                 node.log( "(isNotString) Array to string" );
+
+                var reentrada = false;
+                for ( var x = 0, y = 0 ;(!reentrada) && (x < msg.payload.length) ; x++) {
+                    if (msg.payload[x] !== undefined) {
+                        reentrada = (msg.payload[x].key !== undefined);
+                    }
+                }
+                //console.log( 'reentrada:', reentrada );
+                if (reentrada){
+                    //console.dir( msg.payload );
+                    for ( var x = 0, y = 0 ; x < msg.payload.length ; x++) {
+                        //console.log( 'x:',x);
+                        if (msg.payload[x] !== undefined) {
+                            var item = [ parseInt(msg.payload[x].key) ,msg.payload[x].value ];    
+                            //console.log( item );
+                            msg.payload[y++] = item;
+                        }
+                    }
+                    for ( var x = msg.payload.length ; x >= y; x--){
+                        msg.payload[x] = null;
+                        msg.payload.splice(x, 1);
+                    }
+                    msgS2 = msg.payload;
+                    //console.dir( msgS2 );
+                }                
                 var packedMessageS2 = messageS2.packSync(msgS2);
                 var _msg ='';
                 for (var x = 0 ; x < packedMessageS2.length ; x++) {
                     _msg = _msg + packedMessageS2[ x ];
                 }
-                msg.payload = _msg;
+                msg.payload = _msg;;
             } else {
                 node.log( "(isString) Msg to array" );
                 var msgR = new Buffer( msgS2, "hex"); // desmonta iso8583 do texto, devolvendo array
